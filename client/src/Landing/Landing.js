02577330b0b3bb5ed0db666403c0fd2712f4806c
elthsu <elthsu@ucla.edu>
2018-03-20 13:31:01 -0700

    redirect take 3

--- client/src/Landing/Landing.js
+++ client/src/Landing/Landing.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Sound from "react-sound";
-import { Link } from "react-router-dom";
+import { Redirect } from 'react-router'
 // import FacebookLogin from 'react-facebook-login';
 // <FacebookLogin
 //   appId="1631031436976440"
@@ -49,7 +49,7 @@ class Landing extends Component {
     fx.src = "sounds/dont_underestimate.wav";
     fx.play();
     setTimeout(function(){
-      <Link to="/ThroneRoom"/>;
+      <Redirect to="/ThroneRoom"/>;
     },3000)
   };
 
