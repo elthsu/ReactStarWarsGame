bc63d311c34ef0c1a98fbf99074d8a88a91ddb3a
Elton Hsu <elthsu@ucla.edu>
2018-03-20 12:41:54 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const User = require("./models/user.js");
 const app = express();
